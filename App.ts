0df981767b4e559f27d7a367de6259f77668f361
ankush.bhardwaj0@gmail.com <ankush.bhardwaj0@gmail.com>
2021-05-13 00:01:18 +0530

    feat: get chat from user

--- App.ts
+++ App.ts
@@ -7,6 +7,7 @@ import {
   logVerbosityLevel,
 } from "./config";
 
+// logout action
 const logOutBtn: any = document.getElementById("logout");
 logOutBtn.onclick = () => {
   airgram.api
@@ -15,6 +16,7 @@ logOutBtn.onclick = () => {
     .catch((err) => console.log("Error", err));
 };
 
+// send message action
 const sendMessageBtn: any = document.getElementById("send");
 sendMessageBtn.onclick = () => {
   const fullName = (<HTMLInputElement>document.getElementById("fullName"))
@@ -23,6 +25,15 @@ sendMessageBtn.onclick = () => {
     ?.value as any;
   const [firstName, lastName] = fullName.split(" ");
   sendMessage(message, { firstName, lastName });
+};
+
+// get chats action
+const getChatsBtn: any = document.getElementById("fetchChatBtn");
+getChatsBtn.onclick = () => {
+  const fullName = (<HTMLInputElement>document.getElementById("chatName"))
+    ?.value as any;
+  const [firstName, lastName] = fullName.split(" ");
+  getChatHistory({ firstName, lastName });
 };
 
 const airgram = new Airgram({
@@ -96,17 +107,22 @@ const sendActualTextMsg = async (id: number, textMsg: string) => {
     .catch((e) => console.log("ERROR", e));
 };
 
+const getProperFullName = (firstName: string, lastName: string) => {
+  let fullName: string = "";
+  if (firstName !== undefined && lastName !== undefined)
+    fullName = firstName + " " + lastName;
+  else if (lastName == undefined) fullName = firstName;
+  return fullName;
+};
+
+// gets the chat Id and then sends message
 const getChatId = async (
   chatIdsList: any,
   contactObj: contact,
   textMsg: string
 ) => {
   let { firstName, lastName } = contactObj;
-  let fullName: string = "";
-  if (firstName !== undefined && lastName !== undefined)
-    fullName = firstName + " " + lastName;
-  else if (lastName == undefined) fullName = firstName;
-  console.log(firstName, lastName, fullName);
+  let fullName: string = getProperFullName(firstName, lastName);
   await chatIdsList.forEach(async (chatId: number) => {
     await airgram.api
       .getChat({ chatId: chatId })
@@ -121,15 +137,64 @@ const getChatId = async (
   });
 };
 
+// get chat history
+const getChatHistory = async (contactObj: contact) => {
+  const list: ChatListUnion = { _: "chatListMain" };
+  const chatIdsList = await getChats(list);
+  let { firstName, lastName } = contactObj;
+  let fullName: string = getProperFullName(firstName, lastName);
+  await chatIdsList.forEach(async (chatId: number) => {
+    await airgram.api
+      .getChat({ chatId: chatId })
+      .then(async ({ response }) => {
+        let { title, id } = response as any;
+        if (title.startsWith(fullName)) {
+          await airgram.api
+            .getChatHistory({
+              chatId: id,
+              fromMessageId: 1,
+              offset: -99,
+              limit: 100,
+              onlyLocal: false,
+            })
+            .then(({ response }) => {
+              let { messages } = response as any;
+              messages.forEach((msg: any) => {
+                let { isOutgoing } = msg as any;
+                if (
+                  msg.content !== undefined &&
+                  msg.content.text !== undefined
+                ) {
+                  let msgText = `${isOutgoing ? "YOU" : firstName} : ${
+                    msg.content.text.text
+                  }`;
+                  addMessageToDOM(msgText);
+                }
+              });
+            })
+            .catch((err) => console.log("ERROR", err));
+        }
+      })
+      .catch((e) => console.log(e));
+  });
+};
+
 const sendMessage = async (textMsg: string, contactObj: contact) => {
   const list: ChatListUnion = { _: "chatListMain" };
   let chatIdsList = await getChats(list);
   await getChatId(chatIdsList, contactObj, textMsg);
 };
 
-const addToDOM = (str: string) => {
+const addContactsToDOM = (str: string) => {
   let list = document.getElementById("contacts") as HTMLElement;
   let newItem = document.createElement("li") as HTMLLIElement;
+  newItem.textContent = str;
+  list.appendChild(newItem);
+};
+
+const addMessageToDOM = (str: string) => {
+  let list = document.getElementById("chatMessages") as HTMLElement;
+  let newItem = document.createElement("li") as HTMLElement;
   newItem.textContent = str;
   list.appendChild(newItem);
 };
@@ -155,7 +220,7 @@ airgram.use(async (ctx, next) => {
       if (contactObj.phoneNumber !== "") {
         let contactString: string = `${contactObj.firstName} ${contactObj.lastName}:
         +${contactObj.phoneNumber}`;
-        addToDOM(contactString);
+        addContactsToDOM(contactString);
       }
     }
   }
